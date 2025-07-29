23753cdb34a4248ed7875cdfead36c4b1f64ea11
kevin12441 <kevinsanchezduran4@gmail.com>
2025-07-29 16:09:28 +0000

    creacion del proyecto

---
+++ src/test/java/PymeLogic/PymeLogicApplicationTests.java
@@ -0,0 +1,13 @@
+package PymeLogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PymeLogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
